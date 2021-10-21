0fc6aaf1d8ed2f2d76def86172afa3e01fd0aa0a
shahidur24 <shahidur2424@gmail.com>
2021-10-21 13:40:14 +0600

    update2

--- src/com/exam/TestSpring.java
+++ src/com/exam/TestSpring.java
@@ -22,10 +22,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class TestSpring {
 
+    public static ApplicationContext ctx;
+    public static StudentDao dao;
+    public static ArrayList<Student> list;
+
     public static void main(String[] args) {
         //ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:com/exam/appConfig.xml");
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("com/exam/appConfig.xml");
-        StudentDao dao = (StudentDao) ctx.getBean("dao");
+        ctx = new ClassPathXmlApplicationContext("com/exam/appConfig.xml");
+        dao = (StudentDao) ctx.getBean("dao");
         //insert
         //Student s = new Student(103, "abc", "abc@gmail.com");
         //dao.doAdd(s);
@@ -37,17 +41,36 @@ public class TestSpring {
 
             //String d = df.format(c);
             //Date d2=java.sql.Date.valueOf(d);
-            Student s2 = new Student(101, "xyz", "xyzss@gmail.com", c);
+            //delete
+            //Student s3 = new Student(101, "abc", "abc@gmail.com");
+            //dao.doDel(s3);
+            
+            //Student s2 = new Student(101, "xyz", "xyzss@gmail.com", c);
             //Student s2 = new Student(101, "xyz", "xyzss@gmail.com", c2);
-
-            dao.doUp(s2);
+            //insert(s2);
+            Student s2 = new Student(101, "xyz2", "xyzss@gmail.com", c2);
+            //update(s2);
+            //delete(s2);
+            show();
         } catch (ParseException ex) {
             Logger.getLogger(TestSpring.class.getName()).log(Level.SEVERE, null, ex);
         }
-        //delete
-        //Student s3 = new Student(101, "abc", "abc@gmail.com");
-        //dao.doDel(s3);
-        ArrayList<Student> list = (ArrayList<Student>) dao.allStudent();
+    }
+
+    public static void insert(Student st) {
+        dao.doAdd(st);
+    }
+
+    public static void update(Student st) {
+        dao.doUp(st);
+    }
+
+    public static void delete(Student st) {
+        dao.doDel(st);
+    }
+
+    public static void show() {
+        list = (ArrayList<Student>) dao.allStudent();
         for (Student student : list) {
             System.out.println(student.getId() + "   " + student.getName() + "   " + student.getEmail() + " " + student.getDob());
 
